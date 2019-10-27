55f6fe3a4864464228e73c5f27389fdef68de90d
Krystian <krystian_konecki@wp.pl>
2019-10-27 19:43:09 +0100

    Adding header

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+//Navbar
+const barsButton = document.querySelector('.header__barButtonIcon');
+const headerNav = document.querySelector('.header__nav');
+
+
+barsButton.addEventListener('click', () => {
+    headerNav.classList.toggle('nav--active')
+})
+
+
+
+
+
+
+
+
+})
