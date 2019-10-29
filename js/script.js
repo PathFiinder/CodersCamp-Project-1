595e87ad47bd433fbae16432d04928e599d29565
Krystian <krystian_konecki@wp.pl>
2019-10-29 17:12:41 +0100

    Adding animations and scroll function

--- js/script.js
+++ js/script.js
@@ -9,6 +9,48 @@ barsButton.addEventListener('click', () => {
     headerNav.classList.toggle('nav--active')
 })
 
+//Scroll
+function toScroll(PosTop, navH) {
+    $("body, html").animate(
+      {
+        scrollTop: PosTop - navH
+      },
+      1000
+    );
+    
+    headerNav.classList.remove("nav--active");
+  }
+
+  let headerHeight = $(".header").height();
+  let aboutMePosTop = $(".about-me").offset().top;
+  let skillsPosTop = $(".skills").offset().top;
+  let portfolioPosTop = $(".portfolio").offset().top;
+  let contactPosTop = $(".contact").offset().top;
+
+
+  $(".nav__link--aboutMe").on("click", function() {
+    toScroll(aboutMePosTop, headerHeight);
+  });
+
+  $(".nav__link--skills").on("click", function() {
+    toScroll(skillsPosTop, headerHeight);
+  });
+
+  $(".nav__link--portfolio").on("click", function() {
+    toScroll(portfolioPosTop, headerHeight);
+  });
+
+  $(".nav__link--contact").on("click", function() {
+    toScroll(contactPosTop, headerHeight);
+  });
+
+  window.onresize = () => {
+    headerHeight = $(".header").height();
+    aboutMePosTop = $(".aboutMe").offset().top;
+    skillsPosTop = $(".skills").offset().top;
+    portfolioPosTop = $(".portfolio").offset().top;
+    contactPosTop = $(".contact").offset().top;
+  };
 
 
 
